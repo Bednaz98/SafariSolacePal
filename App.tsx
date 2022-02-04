69aba477b9bbb3239ed1218efb01fcd922583fde
Bednaz98 <30347432+Bednaz98@users.noreply.github.com>
2022-02-04 17:21:03 -0500

    inital setup

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState } from 'react';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [pageIndex, setPageIndex] = useState(0);
+  function switchPage(){
+    switch(pageIndex){
+      case                      0:{return (<><Text>Default page</Text></>)}
+      case                      1:{return (<><Text>Page 2</Text></>)}
+      case /*Josh testing*/     2:{return (<><Text>josh</Text></>)}
+      case /*Brandon Testing*/  3:{return (<><Text>Brandon</Text></>)}
+      case /*Kris Testing*/     4:{return (<><Text>Kris</Text></>)}
+      case /*John Testing*/     5:{return (<><Text>John</Text></>)}
+
+    }
+  }
+
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {switchPage()}
     </View>
   );
 }
