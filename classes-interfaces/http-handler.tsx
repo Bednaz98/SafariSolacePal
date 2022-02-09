c592c1621591951f582321a8ff446bc9db171e1c
akromatikus <31263630+akromatikus@users.noreply.github.com>
2022-02-08 18:39:01 -0600

    'functionality done'
    have not tested yet

--- classes-interfaces/http-handler.tsx
+++ classes-interfaces/http-handler.tsx
@@ -4,39 +4,41 @@ import LocalHandlerInterface from "./local-h-interface";
 import LocalHandler from "./localhandler";
 import axios from "axios"
 
-interface httphandlerInterface{
+export interface httphandlerInterface{
 
-    /** Get all information for the user's reservation */
+    /** Get all information for the user's reservation by ID */
     getReservations(id: string): Promise<Reservation>
 
     /** See all activities which are available.
-     * @param id If an optional ID is supplied, the acitivities for just the user will be fetched
+     * @param id If an optional ID is supplied, the acitivities for just the user will be fetched, otherwise all activities will be fetched
      */
-    getActivities(id?: string) : Promise<Activity | Activity[]>
+    getActivities(id?: string) : Promise< Activity | Activity[] >
 
-    /** Get all room offerings */
-    getRoomOfferings(id? : string): Promise<Offering[]>
+    /** Get all room offerings
+     * @param id If included as an arguement, this will get offerings for only this user
+     */
+    getRoomOfferings(id? : string): Promise< Offering[] >
 
     /** get all room service requests available 
-     * @param id Will return only services that the user has requested
+     * @param id Optional: Will return only services that the user has requested
     */
-    getRoomServiceRequests(id? : string): Promise<ServiceRequest[] | ServiceRequest>
+    getRoomServiceRequests(id? : string): Promise< ServiceRequest | ServiceRequest[] >
 
-    /** Create a new service request for this user */
+    /** Create or delete a service request for this user */
     postServiceRequest(request: ServiceRequest): Promise<boolean>
-
     cancelServiceRequest(request: ServiceRequest): Promise<any> 
 
+    /** Sync front and backend by fetching backend data and updating local context */
     syncApp() : Promise<boolean>
 }
 
 export default class httpHandler implements httphandlerInterface{
 
+    /////////////////////////////////////////////
     private useURL:string = "http://20.124.74.192:3000";
     private devMode:boolean = false;
-    /////////////////////////////////////////////
     private context = useContext(appContext);
-    private localHander: LocalHandlerInterface = new LocalHandler();
+    private localHandler: LocalHandlerInterface = new LocalHandler();
     
     /**this function returns the URL to work with, if devMod is set to false, 
     * it will return the production URL, if true, it will return 'http//localhost:[port]'*/
@@ -49,46 +51,55 @@ export default class httpHandler implements httphandlerInterface{
     constructor(dev:boolean = false){
         this.devMode=dev;
     }
+    //////////////////////////////////////////////
 
     /** get a reservation by ID */
     async getReservations(id: string){
         const response = await axios.get(`${this.getURL()}/reservations/:${id}`);
         const data: Reservation = response.data;  
-        this.localHander.setLocalReservation(data);                  
+        //this.localHandler.setLocalReservation(data);                  
         return data;
     }
 
-    /** Get all activities or a single activity by ID */
-    async getActivities(id?: string): Promise<Activity | Activity[]> {
+    async getActivities(id?: string): Promise< Activity | Activity[] > {
         let response: any
-        if (id) {response = await axios.get(`${this.getURL()}/activities/:${id}`)} 
-        else {response = await axios.get(`${this.getURL()}/activities`); }
-  
-        const data = response.data
-        return data;
+        if (id) {
+            response = await axios.get(`${this.getURL()}/activities/:${id}`)
+            let data = response.data as Activity
+            return data
+        } 
+        else {
+            response = await axios.get(`${this.getURL()}/activities`);
+            let data = response.data as Activity[] 
+            return data
+        }
     }
 
     async getRoomOfferings(id? : string): Promise<Offering[]> {
-        if (id !== null){
-            const response = await this.getRoomServiceRequests(this.context.reservationData.id)
-            //const userOfferings = response.requestedOfferings
+        if (id){
+            const serviceRequest = await this.getRoomServiceRequests(id)
+            const offerings = serviceRequest as ServiceRequest
+            return offerings.requestedOffering
         }
-        const response = await axios.get(`${this.getURL()}/offerings`) 
-        return response.data
+        else{
+            const response = await axios.get(`${this.getURL()}/offerings`)
+            const offerings = response.data as Offering[]
+            return offerings
+        } 
     }
 
-    async getRoomServiceRequests(id? : string): Promise<ServiceRequest[] | ServiceRequest> {
+    async getRoomServiceRequests(id? : string): Promise< ServiceRequest | ServiceRequest[] > {
         let response: any
-        if (id) {response = await axios.get(`${this.getURL()}/servicerequests/:${id}`)} 
-        else {response = await axios.get(`${this.getURL()}/servicerequests`)} 
-        const data: ServiceRequest[] = response.data
-        return data
-    }
-
-    async getRoomServiceRequest(id : string): Promise<ServiceRequest> {
-        const response = await axios.get(`${this.getURL()}/servicerequests/:${id}`)
-        const data: ServiceRequest = response.data
-        return data
+        if (id) {
+            response = await axios.get(`${this.getURL()}/servicerequests/:${id}`)
+            const data = response.data as ServiceRequest; 
+            return data;
+        } 
+        else {
+            response = await axios.get(`${this.getURL()}/servicerequests`) 
+            const data = response.data as ServiceRequest[]; 
+            return data;
+        }
     }
 
     async postServiceRequest(request: ServiceRequest): Promise<boolean> {
@@ -101,43 +112,19 @@ export default class httpHandler implements httphandlerInterface{
 
     async cancelServiceRequest(request: ServiceRequest): Promise<any> {
         //fetcher...
+        await axios.post(`${this.getURL()}/servicerequests`, request)
         const response = 'derp'
         return (response)
     }
 
-    /** Dual functionality: updates local states, but also updates backend */
     async syncApp(){
-        // const localHandler: LocalHandlerInterface = new LocalHandler 
-        
-        // //get'n'set reservation by ID
-        // localHandler.setLocalReservation(await this.getReservations(this.context.reservationData.id))
-        // //localHandler.setLocalOfferings(await this.getRoomOfferings())
-        // const serviceRequests = await this.getRoomServiceRequests(this.context.reservationData.id)
-        // //localHandler.setUserOfferings(serviceRequests)
-        
-        // //const usersToSave: LocalEmployee[] = this.context.employeeList.filter(e => e.status === Status.add);
-        // // usersToSave.forEach(async e => {
-        // //     console.log(e.serverData);
-        // //     console.log("Making post call for:", e.serverData)
-        // //     const tempEmployee = {
-        // //         isManager: e.serverData.isManager,
-        // //         fname: e.serverData.fname,
-        // //         lname: e.serverData.lname,
-        // //         username: e.serverData.username,
-        // //         password: e.serverData.password
-        // //     };
-        // //     const response = await axios.post(`${this.getURL()}/employees`, tempEmployee);
-        // //     const currentEmployee: Employee = response.data; 
-        // //     return(currentEmployee);
-        // // });
-        // // const serverEmployees = await this.getServerAllEmployees();
-        // // this.localHander.syncEmployees(serverEmployees);
-        // // this.context.setSync(true);
+
+        //get-n-set
+        this.localHandler.setLocalReservation(await this.getReservations(this.context.reservationData.id))
+        this.localHandler.setLocalOfferings(await this.getRoomOfferings())
+        this.localHandler.setUserOfferings(await this.getRoomOfferings(this.context.reservationData.id))
         return (true)
-
     }
-
-
 } 
 
 
