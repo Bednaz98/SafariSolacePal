20bbed42b34ac4629afec3b9c6e57410d547d44a
akromatikus <31263630+akromatikus@users.noreply.github.com>
2022-02-13 15:38:35 -0600

    injected tools
    added developer tools to gitignore, only 2 extra files should be added.

---
+++ SafariSolaceStyleTools/get-style.tsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import { ssStyleSheet } from "../developer-styling-tools/ss-stylesheet";
+import { coolTheme, defaultThemeStyle } from "./stylesheet";
+import { themeContext } from "./themecontext";
+
+export enum Theme{
+    default,
+    other,
+    theme2
+}
+
+/** Get the style based off of a ID key. The styles can represent an ID (like a 'syncButton' style), or a UI type (like a 'Text' style)
+ * @param keys The key representing the type of style desired, based off of the ss-style-keys
+ */
+export default function GetStyle(keys: string){
+    //console.log("GET STYLES CALLED")
+
+    const developermode: boolean = false
+
+    if (developermode){
+        return ssStyleSheet(keys)
+    }
+    else{
+        const themes = useContext(themeContext)
+        switch (themes.theme){
+            case (Theme.other) : return coolTheme[keys]
+            default: return defaultThemeStyle[keys]
+        }
+    }
+}
